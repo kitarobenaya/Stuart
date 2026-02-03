c5d3ebb9f7daf43d976773fc3c8e9178244ae5c8
kitarobenaya <kitarobenaya09@gmail.com>
2026-02-03 07:07:59 +0700

    revisi dikit

--- resources/views/dashboard/form.blade.php
+++ resources/views/dashboard/form.blade.php
@@ -14,8 +14,8 @@
         <h1 class="text-[1.5rem] font-semibold">Add a Schedule</h1>
 
         <div class="input-wrapper w-full flex flex-col rounded-2xl p-4">
-            <label for="title" class="text-lg font-semibold">Title</label>
-            <input type="text" name="title" id="title" placeholder="Learn Laravel ..." class="bg-lightPrimary p-1 w-full h-8 text-base placeholder:text-gray-400 focus:outline-lightAccent focus:outline-2 rounded-lg">
+            <label for="date" class="text-lg font-semibold">Date</label>
+            <input type="date" name="date" id="date" class="bg-lightPrimary p-1 w-full text-base placeholder:text-gray-400 focus:outline-lightAccent focus:outline-2 rounded-lg">
         </div>
 
         <div class="input-wrapper w-full flex flex-col rounded-2xl p-4">
@@ -23,16 +23,11 @@
             <textarea name="description" id="description" placeholder="Enter description ..." cols="10" rows="5" class="bg-lightPrimary p-1 w-full text-base placeholder:text-gray-400 focus:outline-lightAccent focus:outline-2 rounded-lg"></textarea>
         </div>
 
-        <div class="input-wrapper w-full flex flex-col rounded-2xl p-4">
-            <label for="date" class="text-lg font-semibold">Description</label>
-            <input type="date" name="date" id="date" class="bg-lightPrimary p-1 w-full text-base placeholder:text-gray-400 focus:outline-lightAccent focus:outline-2 rounded-lg">
-        </div>
-
         <button type="submit" class="bg-lightPrimary w-[40%] h-8 rounded-xl font-semibold outline-2 outline-lightBorder">Add</button>
     </form>
     <script>
-        const title = document.getElementById("title");
-        title.focus();
+        const date = document.getElementById("date");
+        date.focus();
         window.scrollTo(0, 0);
     </script>
 @endsection
